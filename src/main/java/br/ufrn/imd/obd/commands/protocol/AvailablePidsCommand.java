c060357b2b0a42cd39e14a9dde5f4195d9d3126a
Elton Viana <eltonvs@outlook.com>
2018-02-02 15:13:54 -0300

    Fix AvailablePidsCommand to return a List with the list of available commands

--- src/main/java/br/ufrn/imd/obd/commands/protocol/AvailablePidsCommand.java
+++ src/main/java/br/ufrn/imd/obd/commands/protocol/AvailablePidsCommand.java
@@ -12,13 +12,22 @@
  */
 package br.ufrn.imd.obd.commands.protocol;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.ufrn.imd.obd.commands.ObdCommand;
 import br.ufrn.imd.obd.commands.PersistentCommand;
+import br.ufrn.imd.obd.enums.AvailableCommand;
+import br.ufrn.imd.obd.enums.CommandsConstants;
 import br.ufrn.imd.obd.enums.PidRange;
 
 /**
  * Retrieve available PIDs ranging from 21 to 40.
  */
 public class AvailablePidsCommand extends PersistentCommand {
+
+    private List<Class<? extends ObdCommand>> supportedCommands = new ArrayList<>();
 
     /**
      * Default constructor.
@@ -43,7 +52,17 @@ public class AvailablePidsCommand extends PersistentCommand {
      */
     @Override
     protected void performCalculations() {
-
+        supportedCommands.clear();
+        String bits = getBinaryStringHex(getCalculatedResult());
+        int padding = 0;
+        if (cmd != null) {
+            if (cmd == AvailableCommand.PIDS_21_40) {
+                padding = 0x20;
+            } else if (cmd == AvailableCommand.PIDS_41_60) {
+                padding = 0x40;
+            }
+        }
+        fillAvailableCommands(bits, padding);
     }
 
     /**
@@ -51,7 +70,11 @@ public class AvailablePidsCommand extends PersistentCommand {
      */
     @Override
     public String getFormattedResult() {
-        return getCalculatedResult();
+        StringBuilder sb = new StringBuilder();
+        for (Class<? extends ObdCommand> cls : supportedCommands) {
+            sb.append(cls.getName()).append(' ');
+        }
+        return "[ " + sb.toString() + "]";
     }
 
     /**
@@ -61,6 +84,37 @@ public class AvailablePidsCommand extends PersistentCommand {
     public String getCalculatedResult() {
         // First 4 characters are a copy of the command code, don't return those
         return String.valueOf(rawData).substring(4);
+    }
+
+    private void fillAvailableCommands(String bits, int pad) {
+        bits = fillBits(bits);
+
+        for (int i = 0; i < bits.length(); i++) {
+            int pid = i + pad + 1;
+            if (bits.charAt(i) == '1' && CommandsConstants.SUPPORTED_COMMANDS.containsKey(pid) && i != 0x1F) {
+                supportedCommands.add(CommandsConstants.SUPPORTED_COMMANDS.get(pid));
+            }
+        }
+    }
+
+    private String getBinaryStringHex(String hexString) {
+        Long result = Long.parseLong(hexString, 16);
+        return Long.toBinaryString(result);
+    }
+
+    private String fillBits(String str) {
+        int size = 32 - str.length();
+        if (size == 0) {
+            return str;
+        }
+
+        char[] repeat = new char[size];
+        Arrays.fill(repeat, '0');
+        return new String(repeat) + str;
+    }
+
+    public final List<Class<? extends ObdCommand>> getSupportedCommands() {
+        return supportedCommands;
     }
 
 }
---
+++ src/main/java/br/ufrn/imd/obd/enums/CommandsConstants.java
@@ -0,0 +1,91 @@
+package br.ufrn.imd.obd.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.ufrn.imd.obd.commands.ObdCommand;
+import br.ufrn.imd.obd.commands.control.DistanceMILOnCommand;
+import br.ufrn.imd.obd.commands.control.DistanceSinceCCCommand;
+import br.ufrn.imd.obd.commands.control.DtcNumberCommand;
+import br.ufrn.imd.obd.commands.control.EquivalentRatioCommand;
+import br.ufrn.imd.obd.commands.control.ModuleVoltageCommand;
+import br.ufrn.imd.obd.commands.control.TimeSinceCCCommand;
+import br.ufrn.imd.obd.commands.control.TimeSinceMILOnCommand;
+import br.ufrn.imd.obd.commands.control.TimingAdvanceCommand;
+import br.ufrn.imd.obd.commands.engine.AbsoluteLoadCommand;
+import br.ufrn.imd.obd.commands.engine.LoadCommand;
+import br.ufrn.imd.obd.commands.engine.MassAirFlowCommand;
+import br.ufrn.imd.obd.commands.engine.OilTempCommand;
+import br.ufrn.imd.obd.commands.engine.RPMCommand;
+import br.ufrn.imd.obd.commands.engine.RelativeThrottlePositionCommand;
+import br.ufrn.imd.obd.commands.engine.RuntimeCommand;
+import br.ufrn.imd.obd.commands.engine.SpeedCommand;
+import br.ufrn.imd.obd.commands.engine.ThrottlePositionCommand;
+import br.ufrn.imd.obd.commands.fuel.ConsumptionRateCommand;
+import br.ufrn.imd.obd.commands.fuel.EthanolLevelCommand;
+import br.ufrn.imd.obd.commands.fuel.FindFuelTypeCommand;
+import br.ufrn.imd.obd.commands.fuel.FuelLevelCommand;
+import br.ufrn.imd.obd.commands.fuel.FuelTrimCommand;
+import br.ufrn.imd.obd.commands.fuel.WidebandAirFuelRatioCommand;
+import br.ufrn.imd.obd.commands.pressure.BarometricPressureCommand;
+import br.ufrn.imd.obd.commands.pressure.FuelPressureCommand;
+import br.ufrn.imd.obd.commands.pressure.FuelRailPressureCommand;
+import br.ufrn.imd.obd.commands.pressure.IntakeManifoldPressureCommand;
+import br.ufrn.imd.obd.commands.protocol.AvailablePidsCommand;
+import br.ufrn.imd.obd.commands.temperature.AirIntakeTemperatureCommand;
+import br.ufrn.imd.obd.commands.temperature.AmbientAirTemperatureCommand;
+import br.ufrn.imd.obd.commands.temperature.EngineCoolantTemperatureCommand;
+
+/**
+ * Created by elton on 01/02/18.
+ */
+
+public class CommandsConstants {
+    public static final Map<Integer, Class<? extends ObdCommand>> SUPPORTED_COMMANDS = new HashMap<>();
+
+    static {
+        // 01 to 20
+        SUPPORTED_COMMANDS.put(0x01, DtcNumberCommand.class);
+        SUPPORTED_COMMANDS.put(0x04, LoadCommand.class);
+        SUPPORTED_COMMANDS.put(0x05, EngineCoolantTemperatureCommand.class);
+        SUPPORTED_COMMANDS.put(0x06, FuelTrimCommand.class);
+        SUPPORTED_COMMANDS.put(0x07, FuelTrimCommand.class);
+        SUPPORTED_COMMANDS.put(0x08, FuelTrimCommand.class);
+        SUPPORTED_COMMANDS.put(0x09, FuelTrimCommand.class);
+        SUPPORTED_COMMANDS.put(0x0A, FuelPressureCommand.class);
+        SUPPORTED_COMMANDS.put(0x0B, IntakeManifoldPressureCommand.class);
+        SUPPORTED_COMMANDS.put(0x0C, RPMCommand.class);
+        SUPPORTED_COMMANDS.put(0x0D, SpeedCommand.class);
+        SUPPORTED_COMMANDS.put(0x0E, TimingAdvanceCommand.class);
+        SUPPORTED_COMMANDS.put(0x0F, AirIntakeTemperatureCommand.class);
+        SUPPORTED_COMMANDS.put(0x10, MassAirFlowCommand.class);
+        SUPPORTED_COMMANDS.put(0x11, ThrottlePositionCommand.class);
+        SUPPORTED_COMMANDS.put(0x1F, RuntimeCommand.class);
+        SUPPORTED_COMMANDS.put(0x20, AvailablePidsCommand.class);
+
+        // 21 to 40
+        SUPPORTED_COMMANDS.put(0x21, DistanceMILOnCommand.class);
+        SUPPORTED_COMMANDS.put(0x23, FuelRailPressureCommand.class);
+        SUPPORTED_COMMANDS.put(0x2F, FuelLevelCommand.class);
+        SUPPORTED_COMMANDS.put(0x31, DistanceSinceCCCommand.class);
+        SUPPORTED_COMMANDS.put(0x33, BarometricPressureCommand.class);
+        SUPPORTED_COMMANDS.put(0x34, WidebandAirFuelRatioCommand.class);
+        SUPPORTED_COMMANDS.put(0x40, AvailablePidsCommand.class);
+
+        // 41 to 60
+        SUPPORTED_COMMANDS.put(0x42, ModuleVoltageCommand.class);
+        SUPPORTED_COMMANDS.put(0x43, AbsoluteLoadCommand.class);
+        SUPPORTED_COMMANDS.put(0x44, EquivalentRatioCommand.class);
+        SUPPORTED_COMMANDS.put(0x45, RelativeThrottlePositionCommand.class);
+        SUPPORTED_COMMANDS.put(0x46, AmbientAirTemperatureCommand.class);
+        SUPPORTED_COMMANDS.put(0x4D, TimeSinceMILOnCommand.class);
+        SUPPORTED_COMMANDS.put(0x4E, TimeSinceCCCommand.class);
+        SUPPORTED_COMMANDS.put(0x51, FindFuelTypeCommand.class);
+        SUPPORTED_COMMANDS.put(0x52, EthanolLevelCommand.class);
+        SUPPORTED_COMMANDS.put(0x5C, OilTempCommand.class);
+        SUPPORTED_COMMANDS.put(0x5E, ConsumptionRateCommand.class);
+    }
+
+    private CommandsConstants() {
+    }
+}
